ef1cb21451877aa87967939d1097f1f886e2876b
William Gadney <WilliamGadney@discoverybank.co.za>
2018-06-01 14:57:52 +0200

    - Add springfox-bean-validators dependency and configuration - Update Keycloak version

--- src/main/java/za/co/digitalplatoon/invoiceservice/SwaggerConfig.java
+++ src/main/java/za/co/digitalplatoon/invoiceservice/SwaggerConfig.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -28,6 +30,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 // Swagger annotations
 @EnableSwagger2
+@Import({BeanValidatorPluginsConfiguration.class})
 public class SwaggerConfig {
 
     public static final String O_AUTH_2 = "OAuth2";
